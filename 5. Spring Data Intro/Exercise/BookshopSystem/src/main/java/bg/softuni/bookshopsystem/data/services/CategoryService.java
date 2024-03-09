d1838fa07bb4f30643050114465f64407974ee6c
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2024-03-10 01:07:55 +0200

    Update CategoryService.java

--- 5. Spring Data Intro/Exercise/BookshopSystem/src/main/java/bg/softuni/bookshopsystem/data/services/CategoryService.java
+++ 5. Spring Data Intro/Exercise/BookshopSystem/src/main/java/bg/softuni/bookshopsystem/data/services/CategoryService.java
@@ -5,9 +5,7 @@ import bg.softuni.bookshopsystem.data.entities.Category;
 import java.io.IOException;
 
 public interface CategoryService {
-    //нашия контролер ще каже CategoryService seed Categories
-    //и нашия CategoryService ще си намери файла, ще прочете всичко с FileUtil
-    //ще създаде нови категории и ще ги сложи в базата
+   
     void seedCategories() throws IOException;
 
     Category getCategoryById(Long id);
